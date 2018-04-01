b9e7284b784c3ff0a7a7cb7c02ca4f33796a4a17
parksw3 <parksw3@mcmaster.ca>
2018-04-01 12:35:04 -0400

    forgot to commit

--- Project/bifurcation.R
+++ Project/bifurcation.R
@@ -7,44 +7,40 @@ sourceCpp("SIRmodel_npatch.cpp")
 
 L <- load("stochastic.rda")
 
-rinit <- function(param) {
-    with(as.list(param), {
-        ll <- list(
-            S=runif(1, 0, 0.1) * pop,
-            I=runif(1, 0, 0.0001) * pop
-        )
-        
-        ll$R <- pop - ll$S - ll$I
-        
-        ll
-    })
-}
-
-nsim <- 40
+nsim <- 20
 R0vec <- seq(1, 20, by=0.2)
+
+set.seed(101)
+init <- data.frame(
+    S=seq(from=0, to=0.1, length.out=nsim) * base.params[["pop"]],
+    I=seq(from=0, to=0.0001, length.out=nsim) * base.params[["pop"]]
+)
+
+init$R <- base.params[["pop"]] - (init$S + init$I)
+init[] <- apply(init, 2, sample)
 
 blist <- vector('list', length(R0vec))
 
-set.seed(101)
 for (R in R0vec) {
-    print(R)
     
     pp <- base.params
     pp[["R0"]] <- R
-    pp[["nsteps"]] <- 365 * 200 
+    pp[["nsteps"]] <- 365 * 4000 
     
-    blist[[which(R0vec==R)]] <- replicate(nsim, {
-        init <- rinit(pp)
+    blist[[which(R0vec==R)]] <- lapply(1:nsim, function(x){
+        print(paste(R, x))
         
-        df <- SIRmodel_npatch(pp, init, matrix(1), term_time)
+        ii <- init[x,]
         
-        prev <- tail(df$I[df$time%%1==0], 20)/pp[["pop"]]
+        df <- SIRmodel_npatch(pp, ii, matrix(1), term_time)
+        
+        prev <- tail(df$I[df$time%%1==0], 100)/pp[["pop"]]
         
         data.frame(
             prevalence=prev,
             R0=R
         )
-    }, simplify=FALSE)
+    })
     
     save("blist", file="bifurcation.rda")
 }
--- Project/stochastic_fig.R
+++ Project/stochastic_fig.R
@@ -7,65 +7,6 @@ sourceCpp("SIRmodel_npatch.cpp")
 
 L <- load("stochastic.rda")
 
-rinit <- function(param) {
-    with(as.list(param), {
-        ll <- list(
-            S=runif(1, 0, 1) * pop,
-            I=runif(1, 0, 0.0001) * pop
-        )
-        
-        ll$R <- pop - ll$S - ll$I
-        
-        ll
-    })
-}
-
-base.params <- list(
-    R0=17,
-    pop=1e6,
-    b1=0.25,
-    gamma=365/13,
-    mu=0.02,
-    dt=1/365,
-    nsteps=365*100
-)
-
-nsim <- 1
-R0vec <- seq(1, 40, by=0.2)
-
-blist <- vector('list', length(R0vec))
-
-for (R in R0vec) {
-    print(R)
-    
-    pp <- base.params
-    pp[["R0"]] <- R
-    pp[["nsteps"]] <- 365 * 200 
-    
-    blist[[which(R0vec==R)]] <- replicate(nsim, {
-        init <- rinit(pp)
-            
-        df <- SIRmodel_npatch(pp, init, matrix(1), term_time)
-        
-        prev <- tail(df$I[df$time%%1==0], 50)/pp[["pop"]]
-        
-        plot(prev)
-        
-        data.frame(
-            prevalence=prev,
-            R0=R
-        )
-    }, simplify=FALSE)
-    
-}
-
-bdf <- blist %>%
-    lapply(bind_rows) %>%
-    bind_rows %>%
-    mutate(
-        prevalence=prevalence/max(prevalence)
-    )
-
 sdf <- reslist %>%
     bind_rows %>%
     group_by(R0, m) %>%
@@ -76,9 +17,8 @@ sdf <- reslist %>%
     gather(key, value, -R0, -m) %>%
     mutate(m=paste0("m = ", m))
 
-gstoch <- ggplot(bdf) +
-    geom_point(aes(R0, prevalence), shape=".") +
-    geom_line(data=sdf, aes(R0, value, col=key)) +
+gstoch <- ggplot(sdf) +
+    geom_line(aes(R0, value, col=key)) +
     scale_y_continuous("Probability", expand=c(0,0)) +
     scale_x_continuous("Reprouctive number", expand=c(0,0)) +
     facet_wrap(~m) +

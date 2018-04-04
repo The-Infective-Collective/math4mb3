424a84fa4d4999d332e615c68746a337494d1647
zorabf <30967299+zorabf@users.noreply.github.com>
2018-04-04 11:41:22 -0400

    exportR0plot

--- Project/R0plot.R
+++ Project/R0plot.R
@@ -20,9 +20,11 @@ bifur_df_norm <- bifur_df %>%
     mutate(nlp=nlp/max(nlp))
 
 
-ggplot(R0df) +
+gprob <- ggplot(R0df) +
   geom_line(aes(R0, prob.coherence)) +
   facet_wrap(~m, ncol =2) + 
   geom_path(data= bifur_df_norm, aes(R0, nlp, group=interaction(i, j, sim), col=factor(i)), alpha=0.5) +
-  labs(x=TeX('$R_0$'), y='Probability of coherence')
+  labs(x=TeX('$R_0$'), y='Probability of coherence') + 
+  theme_bw()
 
+ggsave("probabilitycoherence.pdf", gprob, width=12, height=5)

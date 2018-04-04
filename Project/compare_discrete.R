03e175ea66ebb3798a001b98836263e697741f2a
parksw3 <parksw3@mcmaster.ca>
2018-04-04 01:53:35 -0400

    last commit before i sleep

---
+++ Project/compare_discrete.R
@@ -0,0 +1,88 @@
+library(Rcpp)
+library(tidyr)
+library(dplyr)
+library(ggplot2); theme_set(theme_bw(base_size = 12,
+                                     base_family = "Times"))
+library(deSolve)
+source("params.R")
+sourceCpp("SIRmodel_npatch.cpp")
+
+if (.Platform$OS.type=="windows") {
+    windowsFonts(Times=windowsFont("Times"))
+} 
+
+SIR <- function(t, y, param) {
+    with(as.list(c(y, param)), {
+        beta <- term_time(t, param)
+        
+        dS <- mu * (pop-S) - beta * S * I
+        dI <- beta * S * I - (mu + gamma) * I
+        dR <- gamma * I - mu * R
+        
+        list(c(dS, dI, dR))
+    })
+}
+
+nyear <- 60
+
+base.params$nsteps <- 365 * nyear
+
+tvec <- seq(0, nyear, by=1/365)
+
+reslist <- vector('list', 3)
+
+iilist <- list(
+    list(
+        S=5000,
+        I=1000,
+        R=1e6-5000-1000
+    ),
+    list(
+        S=10000,
+        I=2000,
+        R=1e6-10000-2000
+    )
+)
+
+for (i in 1:2) {
+    pp <- base.params
+    ii <- iilist[[i]]
+    
+    cont.df <- as.data.frame(deSolve::ode(unlist(ii), tvec, SIR, base.params))
+    disc.df <- SIRmodel_npatch(base.params, ii, matrix(1), term_time)
+    
+    sl <- list(
+        continuous=cont.df[,c("time", "I")],
+        discrete=data.frame(
+            time=disc.df$time,
+            I=disc.df$I
+        )
+    ) %>%
+        bind_rows(.id="type")
+    
+    reslist[[i]] <- sl
+}
+
+rdf <- reslist %>%
+    bind_rows(.id="sim") %>%
+    mutate(
+        sim=factor(sim, levels=c(1,2), 
+                   labels=c("S(0)=5000, I(0)=1000",
+                            "S(0)=10000, I(0)=2000"))
+    )
+
+gcompare <- ggplot(rdf) +
+    geom_line(aes(time, I, col=type, linetype=type),lwd=1.1) +
+    scale_x_continuous("Time (years)") +
+    scale_y_continuous("Prevalence") +
+    scale_color_manual(values=c(1, 2)) +
+    scale_linetype_manual(values=c(1,2)) +
+    facet_wrap(~sim, scale="free", ncol=1) +
+    theme(
+        strip.background = element_blank(),
+        legend.title = element_blank(),
+        legend.position = c(0.85, 0.9),
+        legend.key.width = grid::unit(2, "cm")
+    )
+
+ggsave("compare.pdf", gcompare, width=8, height=5)
